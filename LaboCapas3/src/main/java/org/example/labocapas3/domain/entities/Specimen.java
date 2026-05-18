0f457eedf14f23a091cf85d714b0fbedaf129d60
Juanbermudezp <00218123@uca.edu.sv>
2026-05-18 15:57:58 -0600

    :memo: Modificacion de la clase Specimen

--- LaboCapas3/src/main/java/org/example/labocapas3/domain/entities/Specimen.java
+++ LaboCapas3/src/main/java/org/example/labocapas3/domain/entities/Specimen.java
@@ -1,4 +1,22 @@
 package org.example.labocapas3.domain.entities;
 
+import jakarta.persistence.*;
+import lombok.*;
+import java.util.UUID;
+
+@Entity
+@Table(name = "Specimen")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Specimen {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String name;
+    private String description;
+    private String location;
+    private Integer dangerLevel;
 }
