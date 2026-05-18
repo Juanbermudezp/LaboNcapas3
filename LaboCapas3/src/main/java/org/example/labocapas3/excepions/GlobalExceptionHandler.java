2b145563fb7a590fff813d86d9a37c6005073192
Juanbermudezp <00218123@uca.edu.sv>
2026-05-18 17:50:32 -0600

    :memo: Modificacion de la Expecion Global

--- LaboCapas3/src/main/java/org/example/labocapas3/excepions/GlobalExceptionHandler.java
+++ LaboCapas3/src/main/java/org/example/labocapas3/excepions/GlobalExceptionHandler.java
@@ -1,4 +1,37 @@
 package org.example.labocapas3.excepions;
 
+import org.example.labocapas3.domain.dto.response.ApiErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ApiErrorResponse> handleResourceNotFound(ResourceNotFoundException e) {
+        return buildErrorResponse(HttpStatus.NOT_FOUND, e.getMessage());
+    }
+
+    // Opcional: Manejo genérico para otros errores no controlados
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiErrorResponse> handleGenericException(Exception e) {
+        return buildErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, "An unexpected error occurred in the Sheikah System");
+    }
+
+    public ResponseEntity<ApiErrorResponse> buildErrorResponse(HttpStatus status, Object message) {
+        String uri = ServletUriComponentsBuilder.fromCurrentRequestUri().build().getPath();
+        return ResponseEntity.status(status)
+                .body(ApiErrorResponse.builder()
+                        .status(status.value())
+                        .message(message)
+                        .time(LocalDateTime.now())
+                        .uri(uri)
+                        .build()
+                );
+    }
 }
