f4436630b8c618a2365f4ffab9e88512ee1babc6
Juanbermudezp <00218123@uca.edu.sv>
2026-05-18 17:48:48 -0600

    :memo: Modificacion de la ApiError

--- LaboCapas3/src/main/java/org/example/labocapas3/domain/dto/response/ApiErrorResponse.java
+++ LaboCapas3/src/main/java/org/example/labocapas3/domain/dto/response/ApiErrorResponse.java
@@ -1,4 +1,14 @@
 package org.example.labocapas3.domain.dto.response;
 
+import lombok.Builder;
+import lombok.Data;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
 public class ApiErrorResponse {
+    private Object message;
+    private int status;
+    private LocalDateTime time;
+    private String uri;
 }
