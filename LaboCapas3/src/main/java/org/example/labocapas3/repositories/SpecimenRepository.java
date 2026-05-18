de0ddd0cbe54f4fe8bd6fbaa0c2331da0da5c3d4
Juanbermudezp <00218123@uca.edu.sv>
2026-05-18 17:35:36 -0600

    :memo: Modificacion del Repositorio

--- LaboCapas3/src/main/java/org/example/labocapas3/repositories/SpecimenRepository.java
+++ LaboCapas3/src/main/java/org/example/labocapas3/repositories/SpecimenRepository.java
@@ -1,4 +1,10 @@
 package org.example.labocapas3.repositories;
 
-public class SpecimenRepository {
+import org.example.labocapas3.domain.entities.Specimen;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.UUID;
+
+@Repository
+public interface SpecimenRepository extends JpaRepository<Specimen, UUID> {
 }
