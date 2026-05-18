f1dd30f02ae9b86caf82c2a51dcdfaccc4c10c2f
Juanbermudezp <00218123@uca.edu.sv>
2026-05-18 14:58:37 -0600

    :memo: Creacion del Proyecto

---
+++ LaboCapas3/src/test/java/org/example/labocapas3/LaboCapas3ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.labocapas3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaboCapas3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
