63e5a6e3c111d6cd971a072a00c4fef1feb2ddf4
Juanbermudezp <00218123@uca.edu.sv>
2026-05-18 16:31:56 -0600

    :memo: Modificacion del Response

--- LaboCapas3/src/main/java/org/example/labocapas3/domain/dto/response/specimen/SpecimenResponse.java
+++ LaboCapas3/src/main/java/org/example/labocapas3/domain/dto/response/specimen/SpecimenResponse.java
@@ -1,4 +1,12 @@
 package org.example.labocapas3.domain.dto.response.specimen;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class SpecimenResponse {
+    private String name;
+    private String description;
+    private Integer dangerLevel;
 }
