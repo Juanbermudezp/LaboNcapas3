6a348426027f92217e02132a8bbeda9e0685c086
Juanbermudezp <00218123@uca.edu.sv>
2026-05-18 17:37:36 -0600

    :memo: Creacion del servicio

---
+++ LaboCapas3/src/main/java/org/example/labocapas3/services/SpecimenService.java
@@ -0,0 +1,4 @@
+package org.example.labocapas3.services;
+
+public interface SpecimenService {
+}
---
+++ LaboCapas3/src/main/java/org/example/labocapas3/services/impl/SpecimenServiceImpl.java
@@ -0,0 +1,4 @@
+package org.example.labocapas3.services.impl;
+
+public class SpecimenServiceImpl {
+}
