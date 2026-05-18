283275665b3ca96fe0424f877017f7427d55694c
Juanbermudezp <00218123@uca.edu.sv>
2026-05-18 16:21:27 -0600

    :memo: Modificacion del Request

--- LaboCapas3/src/main/java/org/example/labocapas3/domain/dto/request/specimen/CreateSpecimenRequest.java
+++ LaboCapas3/src/main/java/org/example/labocapas3/domain/dto/request/specimen/CreateSpecimenRequest.java
@@ -1,4 +1,22 @@
 package org.example.labocapas3.domain.dto.request.specimen;
 
+import jakarta.validation.constraints.*;
+import lombok.*;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class CreateSpecimenRequest {
+    @NotBlank(message = "Specimen name cannot be empty.")
+    private String name;
+
+    @NotBlank(message = "Description is required.")
+    private String description;
+
+    private String location;
+
+    @NotNull(message = "Danger level is required.")
+    @Min(1) @Max(10)
+    private Integer dangerLevel;
 }
