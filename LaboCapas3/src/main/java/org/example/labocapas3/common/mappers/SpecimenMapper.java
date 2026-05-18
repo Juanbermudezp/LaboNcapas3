8362acacad4082470369ff1dc00719ad6dc42f42
Juanbermudezp <00218123@uca.edu.sv>
2026-05-18 17:31:52 -0600

    :memo: Modificacion del Mapper y el Pageable

--- LaboCapas3/src/main/java/org/example/labocapas3/common/mappers/SpecimenMapper.java
+++ LaboCapas3/src/main/java/org/example/labocapas3/common/mappers/SpecimenMapper.java
@@ -1,4 +1,44 @@
 package org.example.labocapas3.common.mappers;
 
+import org.example.labocapas3.domain.dto.request.specimen.CreateSpecimenRequest;
+import org.example.labocapas3.domain.dto.response.specimen.PageableResponse;
+import org.example.labocapas3.domain.dto.response.specimen.SpecimenResponse;
+import org.example.labocapas3.domain.entities.Specimen;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Component;
+import java.util.List;
+
+@Component
 public class SpecimenMapper {
+    public Specimen toEntityCreate(CreateSpecimenRequest request) {
+        return Specimen.builder()
+                .name(request.getName())
+                .description(request.getDescription())
+                .location(request.getLocation())
+                .dangerLevel(request.getDangerLevel())
+                .build();
+    }
+
+    public SpecimenResponse toDto(Specimen specimen) {
+        return SpecimenResponse.builder()
+                .name(specimen.getName())
+                .description(specimen.getDescription())
+                .dangerLevel(specimen.getDangerLevel())
+                .build();
+    }
+
+    public PageableResponse<SpecimenResponse> toPageableResponse(Page<Specimen> page) {
+        List<SpecimenResponse> content = page.getContent().stream()
+                .map(this::toDto)
+                .toList();
+
+        return PageableResponse.<SpecimenResponse>builder()
+                .content(content)
+                .pageNumber(page.getNumber())
+                .pageSize(page.getSize())
+                .totalElements(page.getTotalElements())
+                .totalPages(page.getTotalPages())
+                .last(page.isLast())
+                .build();
+    }
 }
--- LaboCapas3/src/main/java/org/example/labocapas3/domain/dto/response/specimen/PageableResponse.java
+++ LaboCapas3/src/main/java/org/example/labocapas3/domain/dto/response/specimen/PageableResponse.java
@@ -1,4 +1,17 @@
 package org.example.labocapas3.domain.dto.response.specimen;
 
-public class PageableResponse {
+import lombok.*;
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class PageableResponse<T> {
+    private List<T> content;
+    private int pageNumber;
+    private int pageSize;
+    private long totalElements;
+    private int totalPages;
+    private boolean last;
 }
